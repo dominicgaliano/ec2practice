8dbddffa3bb1bfbe5c1fcc5469a66308cdebe0c4
Dominic Galiano <dgaliano@utexas.edu>
2023-10-30 12:27:57 -0400

    adding new route to test deployment pipeline

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const path = require("path");
 app.use(morgan("combined"));
 app.use("/", express.static(path.join(__dirname, "public")));
 
+// demo, adding route to test out gh push action
+app.get("/api", (req, res) => {
+  res.status(200);
+  res.send('API route');
+};
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
